dd9db8c325f3a5d7de199d60e0bee6c313511ebb
HyunJoon,Kim <kimkuma04@gmail.com>
2020-11-23 22:51:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Spring boot Rest API 
+
+## Spring boot REST API 
+>  스프링 부트를 이용한 REST API 프로젝트 구성 
+
+## 사용 기술
+* Spring boot 2.4.0
+* Spring REST Docs
+* Spring HATEOAS
+* SPring DATA JPA
+* Lombok
+
+## 개발환경
+> * JDK 11  
+
---
+++ src/test/java/org/dukdns/kuma04/springrestapi/SpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dukdns.kuma04.springrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
