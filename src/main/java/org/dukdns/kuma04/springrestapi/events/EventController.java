6e35a817949e3f54b308b9d486ae44cd2a4c3d94
HyunJoon,Kim <kimkuma04@gmail.com>
2020-11-26 23:48:40 +0900

    profile 추가 및 Rest Docs 문서

--- src/main/java/org/dukdns/kuma04/springrestapi/events/EventController.java
+++ src/main/java/org/dukdns/kuma04/springrestapi/events/EventController.java
@@ -1,6 +1,7 @@
 package org.dukdns.kuma04.springrestapi.events;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -53,7 +54,7 @@ public class EventController {
         EventResource eventResource = new EventResource(event);
         eventResource.add(selfLinkBuilder.withRel("query-events"));
         eventResource.add(selfLinkBuilder.withRel("update-event"));
-
+        eventResource.add(Link.of("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
     }
 }
