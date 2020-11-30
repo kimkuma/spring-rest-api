b9a6b45d756f0e21301cd655b746a6131f201c20
HyunJoon,Kim <kimkuma04@gmail.com>
2020-11-30 22:18:30 +0900

    Spring Security Form setting.

--- src/main/java/org/dukdns/kuma04/springrestapi/SpringRestApiApplication.java
+++ src/main/java/org/dukdns/kuma04/springrestapi/SpringRestApiApplication.java
@@ -1,9 +1,7 @@
 package org.dukdns.kuma04.springrestapi;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRestApiApplication {
@@ -12,8 +10,4 @@ public class SpringRestApiApplication {
 		SpringApplication.run(SpringRestApiApplication.class, args);
 	}
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
 }
